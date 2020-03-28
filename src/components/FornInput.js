9187eacc9bd8607ed18cc605b6f37e1820e82d1a
jantapabh <s6035512034@gmail.com>
2020-03-28 23:23:44 +0700

    add Forminput

---
+++ src/components/FornInput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const FormInput = () => {
+    return(
+        <div>
+            FormInput
+            </div>
+
+    );
+}
+
+export default FormInput;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-const LoginForm = () => {
+const Login = () => {
 
     return (
         <div>
@@ -9,4 +9,4 @@ const LoginForm = () => {
         </div>
     );
 }
-export default LoginForm;
+export default Login;
