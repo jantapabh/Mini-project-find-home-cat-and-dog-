395cdb75fec4ef3bf3a48f09157771291ecfc5ee
jantapabh <s6035512034@gmail.com>
2020-03-28 22:24:01 +0700

    change arrow function this

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-  jantapa. . 
+const App = () => {
+
+return(
+  <div>
+    Hello World
     </div>
-  );
+
+);
+
 }
 
 export default App;
