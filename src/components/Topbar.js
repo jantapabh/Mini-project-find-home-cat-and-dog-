1f0c26884bbe8cfb862e9e4e39bdf1e62fb55ef0
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-31 00:33:25 +0700

    add List Topbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from '../static/images/logo.png'
 import './Topbar.css'
+import { Link } from 'react-router-dom'
 
 
 const Topbar = () => {
@@ -9,11 +10,21 @@ const Topbar = () => {
     return (
         <div>
             <div className="Images">
-                <img src={logo} />
+                <a href="/">
+                    <img src={logo} />
+                </a>
             </div>
+            <div className="menu-list">
             <div>
-                
+                    <a className="link" href="/ListItems">หน้าหลัก</a>
                 </div>
+                <div>
+                    <a className="link" href="/FormInput">กรอกข้อมูล</a>
+                </div>
+                <div>
+                    <a className="link" href="/Register">ลงทะเบียน</a>
+                </div>
+            </div>
         </div>
     );
 }
