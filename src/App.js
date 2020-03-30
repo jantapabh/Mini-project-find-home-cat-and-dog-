d7e8495b78ed0d4054adba73023eb148e243592b
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-31 00:41:02 +0700

    add commit antd

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Topbar from './components/Topbar';
+import { BrowserRouter, Route, Link, Router, Redirect } from 'react-router-dom';
+import { Switch } from 'antd';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import FormInput from './components/FormInput';
+import ListItems from './components/ListItems';
+import Login from './components/Login';
+import Register from './components/Resgister';
+
 
 const App = () => {
 
 return(
   <div>
-   <Topbar />
-    </div>
+  <Topbar />
+  <div>
+    <BrowserRouter>
+      <Route exact path="/" />
+      <Route path="/FormInput" component={FormInput} />
+      <Route path="/ListItems" component={ListItems} />
+      <Route path="/Register" component={Register} />
+      <Route path="/login" component={Login} />
+     
+    </BrowserRouter>
+  </div>
+</div>
+
 );
 
 }
