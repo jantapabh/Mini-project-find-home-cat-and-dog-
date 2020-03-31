d0e156e839dcdd860898ac19149df190517df2e4
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-31 23:58:24 +0700

    add Footer

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 
 const ListItems = () => {
+
     return (
         <div>
-            <div>
+            <div className="Footer">
             </div>
-
         </div>
     );
 }
