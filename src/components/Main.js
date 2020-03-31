a022b60bd6b398303359166cf5c041da5cc1920b
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-31 18:56:30 +0700

    add commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,12 +5,19 @@ import pic6 from '../static/images/pic6.jpg'
 const Main = () => {
 
     return (
-        <div className="Main">
-            <div className="show">
-                <img className="img" src="https://cdn.pixabay.com/photo/2016/01/19/17/41/friends-1149841_1280.jpg" />
-            </div>
-            <div className="Footer">
+        <div>
+            <div className="Main">
+                <div className="show">
+                    <img className="img" src="https://cdn.pixabay.com/photo/2016/01/19/17/41/friends-1149841_1280.jpg" />
+                    <button class="btn">Button</button>
                 </div>
+                <div className="Footer">
+                    <p>Hiiiii</p>
+                </div>
+                {/* <div className="Footer2">
+                    <p>Hiiiii</p>
+                </div> */}
+            </div>
         </div>
     )
 }
