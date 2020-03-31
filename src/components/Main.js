04b25d68bc5f09b041b085f1740286269857780e
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-31 19:47:36 +0700

    add commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,7 +9,7 @@ const Main = () => {
             <div className="Main">
                 <div className="show">
                     <img className="img" src="https://cdn.pixabay.com/photo/2016/01/19/17/41/friends-1149841_1280.jpg" />
-                    <div>
+                    <div className="content">
                         <p className="Param"> " ร่วมเป็นส่วนหนึ่งในการช่วยเหลือสัตว์ไร้บ้าน </p> <br />
                         <p className="Param2"> . . . อย่าปล่อยให้พวกเขาโดเดี่ยว "</p>
                     </div>
