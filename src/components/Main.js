594062798a5318d758987b25ec377a0811d6daac
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-01 00:07:42 +0700

    add css

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,8 @@ const Main = () => {
 
     return (
         <div>
+            <div className="h">
+                </div>
             <div className="Main">
                 <div className="show">
                     <img className="img" src="https://cdn.pixabay.com/photo/2016/01/19/17/41/friends-1149841_1280.jpg" />
