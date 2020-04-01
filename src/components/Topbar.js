845ae7c6ea54e66673630e036563d7a2fdfa0d8f
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-02 01:11:00 +0700

    add topbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -12,26 +12,35 @@ const Topbar = () => {
 
     return (
         <div>
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">CHUAY NONG HAA BAAN</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/ListItems">OBSERVATION ROOM</Nav.Link>
-                    <NavDropdown title="ABOUT" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">LOGIN</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">REGISTER</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">LOGOUT</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                    </NavDropdown>
-                </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-success">Search</Button>
-                </Form>
-            </Navbar.Collapse>
-        </Navbar>
-    </div>
+            <Navbar bg="light" expand="lg">
+
+                <Navbar.Brand href="/">
+                    <img
+                        src={logo1}
+                        width="100"
+                        height="80"
+                        className="d-inline-block align-top"
+                        alt="CAHAUY NONG HAA BAAN"
+                    />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/ListItems" style={{ fontSize: 30}}>OBSERVATION ROOM</Nav.Link>
+                        <NavDropdown title="ABOUT" style={{ fontSize: 30}} id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/Login" style={{ fontSize: 20}}>LOGIN</NavDropdown.Item>
+                            <NavDropdown.Item href="/Register" style={{ fontSize: 20}}>REGISTER</NavDropdown.Item>
+                            <NavDropdown.Item href="/logout" style={{ fontSize: 20}}>LOGOUT</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                        </NavDropdown>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
 
 
     );
