335018480acf71ac11a29b483964231a7e71aa5e
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-01 20:18:09 +0700

    add commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -21,22 +21,22 @@ export const AuthActions = {
         const res = await axios.get(`http://localhost/api/auth`)
         dispatch({ type: 'GET_LOGIN_STATUS', payload: res.data });
     },
-    loginPSU: (username, password) => async (dispatch) => {
-        const name = username + ''
-        const pass = password + ''
-        if (name.length === 10 && pass.length > 6 && username == '6035512034') {
-            const res = await axios.post('http://localhost/api/auth/psu', { username, password })
-            const { stdId, firstname, lastname, id, type } = res.data;
+    // loginPSU: (username, password) => async (dispatch) => {
+    //     const name = username + ''
+    //     const pass = password + ''
+    //     if (name.length === 10 && pass.length > 6 && username == '6035512034') {
+    //         const res = await axios.post('http://localhost/api/auth/psu', { username, password })
+    //         const { stdId, firstname, lastname, id, type } = res.data;
 
-            if (type == ' ') {
-                return alert('username or password incorrect');
-                // alert('username or password incorrect');
-            }
-            else {
-                dispatch({ type: 'LOGIN_PSU', payload: res.data })
-            }
-        }
-    },
+    //         if (type == ' ') {
+    //             return alert('username or password incorrect');
+    //             // alert('username or password incorrect');
+    //         }
+    //         else {
+    //             dispatch({ type: 'LOGIN_PSU', payload: res.data })
+    //         }
+    //     }
+    // },
     logout: () => async (dispatch) => {
         const res = await axios.get(`http://localhost/api/auth/logout`)
         dispatch({ type: 'LOGOUT' })
@@ -45,8 +45,8 @@ export const AuthActions = {
 
 const AuthReducer = (data = initAuthData, action) => {
     switch (action.type) {
+        
         case 'GET_LOGIN_STATUS': return action.payload;
-        case 'LOGIN_PSU': return { ...data, psuInfo: action.payload };
         case 'LOGOUT': return initAuthData
         default: return data
     }
