d5fc6efd603a8066be63e5713c1ced3a0cb5421e
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-01 16:58:30 +0700

    add commit

--- server/animals.js
+++ server/animals.js
@@ -16,3 +16,47 @@ app.use('/api', bodyParser.urlencoded({
     extended: false
 
 }), router);
+
+let animals = [
+
+    {
+        id: 1,
+        imgUrl: 'https://scontent.fbkk8-2.fna.fbcdn.net/v/t1.0-9/90357810_1080609382307203_9018453696849117184_n.jpg?_nc_cat=107&_nc_sid=666b5a&_nc_eui2=AeHLMSr1d6B4pOCjHyx08EIFkvoZFdlydREK9wdsi4cJRGbkw1K2xLzeqEuNIGPGCvSWfEWhKXisTz0tKAq0_T4eYXshmAuhwi7pYRwjyXlGIA&_nc_ohc=RbxPPh73VK8AX80ab2f&_nc_ht=scontent.fbkk8-2.fna&oh=e2e37e4d1760f067755b5116036efe9d&oe=5EA8D419',
+        strain: 'Normal',
+        name: 'Malee',
+        old: 3,
+        habits: 'alone',
+        because: 'ไม่สะดวกเลี้ยง'
+    },
+    {
+        id: 2,
+        imgUrl: 'https://scontent.fbkk8-2.fna.fbcdn.net/v/t1.0-9/90357810_1080609382307203_9018453696849117184_n.jpg?_nc_cat=107&_nc_sid=666b5a&_nc_eui2=AeHLMSr1d6B4pOCjHyx08EIFkvoZFdlydREK9wdsi4cJRGbkw1K2xLzeqEuNIGPGCvSWfEWhKXisTz0tKAq0_T4eYXshmAuhwi7pYRwjyXlGIA&_nc_ohc=RbxPPh73VK8AX80ab2f&_nc_ht=scontent.fbkk8-2.fna&oh=e2e37e4d1760f067755b5116036efe9d&oe=5EA8D419',
+        strain: 'Normal',
+        name: 'Malee',
+        old: 3,
+        habits: 'alone',
+        because: 'ไม่สะดวกเลี้ยง'
+    },
+    {
+        id: 3,
+        imgUrl: 'https://scontent.fbkk8-2.fna.fbcdn.net/v/t1.0-9/90357810_1080609382307203_9018453696849117184_n.jpg?_nc_cat=107&_nc_sid=666b5a&_nc_eui2=AeHLMSr1d6B4pOCjHyx08EIFkvoZFdlydREK9wdsi4cJRGbkw1K2xLzeqEuNIGPGCvSWfEWhKXisTz0tKAq0_T4eYXshmAuhwi7pYRwjyXlGIA&_nc_ohc=RbxPPh73VK8AX80ab2f&_nc_ht=scontent.fbkk8-2.fna&oh=e2e37e4d1760f067755b5116036efe9d&oe=5EA8D419',
+        strain: 'Normal',
+        name: 'Malee',
+        old: 3,
+        habits: 'alone',
+        because: 'ไม่สะดวกเลี้ยง'
+    }
+];
+
+router.route('/animals')
+
+.get((req, res) => res.json(animals))
+.post((req, res) => {
+
+    let animal = {}
+    animal.id = animals[animals.length - 1].id + 1
+    animal.imgUrl = req.body.imgUrl;
+    animal.strain = req.body.strain;
+    
+
+})
