8566b5795c7b1a2b7d0aba7efd1c33f1d8487400
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-06 01:59:27 +0700

    add commit data show

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Button, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
 import './Form.css'
 import { formActions } from '../redux/store'
 import { useSelector, useDispatch } from 'react-redux'
 import { animalActions } from '../redux/store'
 import { bindActionCreators } from 'redux';
 import axios from 'axios'
-import { Form, Col } from 'react-bootstrap';
+import Sidebar from './Sidebar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const FormInput = props => {
@@ -23,100 +26,143 @@ const FormInput = props => {
         actionsAnimal.addAnimal(animals, form)
     }
 
+    const [activeTab, setActiveTab] = useState('1');
+
+    const toggle = tab => {
+        if (activeTab !== tab) setActiveTab(tab);
+    }
+
 
     return (
-        <div>
-            <div className="col-6 mt-5 mx-auto card">
-                <div className="card-body">
-                    <h1 className="Topic">" ADD CAT OR DOG "</h1>
-                </div>
-                <form>
-                    <div className="form-group">
-                        <label htmlFor="imgUrl1">Image 1 : รูปภาพ</label>
-                        <input type="text"
-                            name="imgUrl1"
-                            className="form-control"
-                            id="imgUrl1"
-                            onChange={(e) => actionForm.changeImgurl1(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="imgUrl2">Image 2 : รูปภาพ</label>
-                        <input type="text"
-                            name="imgUrl2"
-                            className="form-control"
-                            id="imgUrl2"
-                            onChange={(e) => actionForm.changeImgurl2(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="imgUrl3">Image 3  : รูปภาพ</label>
-                        <input type="text"
-                            name="imgUrl3"
-                            className="form-control"
-                            id="imgUrl3"
-                            onChange={(e) => actionForm.changeImgurl3(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="strain">Strain : สายพันธ์</label>
-                        <input type="text"
-                            name="strain"
-                            className="form-control"
-                            id="strain"
-                            onChange={(e) => actionForm.changeStrain(e.target.value)}
-                        />
 
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="name">Name : ชื่อ</label>
-                        <input type="text"
-                            name="name"
-                            className="form-control"
-                            id="name"
-                            onChange={(e) => actionForm.changeName(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="old">Old : อายุ</label>
-                        <input type="number"
-                            name="old"
-                            className="form-control"
-                            id="old"
-                            onChange={(e) => actionForm.changeOld(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="habit">Habits : นิสัย</label>
-                        <input type="text"
-                            name="habit"
-                            className="form-control"
-                            id="habit"
-                            onChange={(e) => actionForm.changeHabit(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="because">Because : เหตุผลที่หาบ้านใหม่</label>
-                        <input type="text"
-                            name="because"
-                            className="form-control"
-                            id="because"
-                            onChange={(e) => actionForm.changeBecause(e.target.value)}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="imgUrl">Status : สถานะ</label>
-                        <input type="text"
-                            name="status"
-                            className="form-control"
-                            id="status"
-                            onChange={(e) => actionForm.changeStatus(e.target.value)}
-                        />
-                    </div>
-                    <div className="card-body">
-                    <h1 className="Topic">" ADD YOUR DATA"</h1>
-                </div>
-                    <div className="form-group">
+        <div>
+            <Nav tabs>
+                <NavItem>
+                    <NavLink
+                        className={classnames({ active: activeTab === '1' })}
+                        onClick={() => { toggle('1'); }}
+                    >
+                        ADD DOG OR CAT
+            </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink
+                        className={classnames({ active: activeTab === '2' })}
+                        onClick={() => { toggle('2'); }}
+                    >
+                        ADD YOUR DATA
+            </NavLink>
+                </NavItem>
+            </Nav>
+            <TabContent activeTab={activeTab}>
+                <TabPane tabId="1">
+                    <Row>
+                        <Col sm="12">
+                            <div>
+                                <div>
+                                    <div className="col-6 mt-5 mx-auto card">
+                                        <div className="card-body">
+                                            <h1 className="Topic">" ADD CAT OR DOG "</h1>
+                                        </div>
+                                        <form>
+                                            <div className="form-group">
+                                                <label htmlFor="imgUrl1">Image 1 : รูปภาพ</label>
+                                                <input type="text"
+                                                    name="imgUrl1"
+                                                    className="form-control"
+                                                    id="imgUrl1"
+                                                    onChange={(e) => actionForm.changeImgurl1(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className="form-group">
+                                                <label htmlFor="imgUrl2">Image 2 : รูปภาพ</label>
+                                                <input type="text"
+                                                    name="imgUrl2"
+                                                    className="form-control"
+                                                    id="imgUrl2"
+                                                    onChange={(e) => actionForm.changeImgurl2(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className="form-group">
+                                                <label htmlFor="imgUrl3">Image 3  : รูปภาพ</label>
+                                                <input type="text"
+                                                    name="imgUrl3"
+                                                    className="form-control"
+                                                    id="imgUrl3"
+                                                    onChange={(e) => actionForm.changeImgurl3(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className="form-group">
+                                                <label htmlFor="strain">Strain : สายพันธ์</label>
+                                                <input type="text"
+                                                    name="strain"
+                                                    className="form-control"
+                                                    id="strain"
+                                                    onChange={(e) => actionForm.changeStrain(e.target.value)}
+                                                />
+
+                                            </div>
+                                            <div className="form-group">
+                                                <label htmlFor="name">Name : ชื่อ</label>
+                                                <input type="text"
+                                                    name="name"
+                                                    className="form-control"
+                                                    id="name"
+                                                    onChange={(e) => actionForm.changeName(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className="form-group">
+                                                <label htmlFor="old">Old : อายุ</label>
+                                                <input type="number"
+                                                    name="old"
+                                                    className="form-control"
+                                                    id="old"
+                                                    onChange={(e) => actionForm.changeOld(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className="form-group">
+                                                <label htmlFor="habit">Habits : นิสัย</label>
+                                                <input type="text"
+                                                    name="habit"
+                                                    className="form-control"
+                                                    id="habit"
+                                                    onChange={(e) => actionForm.changeHabit(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className="form-group">
+                                                <label htmlFor="because">Because : เหตุผลที่หาบ้านใหม่</label>
+                                                <input type="text"
+                                                    name="because"
+                                                    className="form-control"
+                                                    id="because"
+                                                    onChange={(e) => actionForm.changeBecause(e.target.value)}
+                                                />
+                                            </div>
+                                            <div className="form-group">
+                                                <label htmlFor="imgUrl">Status : สถานะ</label>
+                                                <input type="text"
+                                                    name="status"
+                                                    className="form-control"
+                                                    id="status"
+                                                    onChange={(e) => actionForm.changeStatus(e.target.value)}
+                                                />
+                                            </div>
+                                        </form>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </TabPane>
+                <TabPane tabId="2">
+                    <Row>
+                        <Col sm="12" >
+                            <div className="col-6 mt-5 mx-auto card">
+                                <div>
+                                    <h1 className="Topic">" ADD YOUR DATA"</h1>
+                                </div>
+                                <div className="form-group">
                                     <label htmlFor="imgUrlUser">IMAGE</label>
                                     <input type="text"
                                         name="imgUrlUser"
@@ -208,12 +254,16 @@ const FormInput = props => {
                                         onChange={(e) => actionForm.changeZipUser(e.target.value)}
                                     />
                                 </div>
-                                <div className="text-center">
-                                    <button className="btn btn-primary my-1" type="submit" onClick={addAnimal}>ADD</button>
-                                </div>
-                </form>
+                            </div>
+                        </Col>
+                    </Row>
+                </TabPane>
+            </TabContent>
+            <div className="text-center">
+            <Button color="primary" onClick={addAnimal} style={{ margin: 15, padding: 10}}>ADD DOG OR CAT AND YOUR DATA</Button>
             </div>
         </div>
+
     );
 }
 
