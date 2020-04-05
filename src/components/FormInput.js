6924315997a6bf074ba4bd4df7a90eb1b199b89d
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-05 19:31:43 +0700

    add commit

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,10 +1,11 @@
-import React, {useState, useEffect }from 'react'
+import React, { useState, useEffect } from 'react'
 import './Form.css'
 import { formActions } from '../redux/store'
 import { useSelector, useDispatch } from 'react-redux'
 import { animalActions } from '../redux/store'
 import { bindActionCreators } from 'redux';
 import axios from 'axios'
+import { Button, Form, Col } from 'react-bootstrap';
 
 
 const FormInput = props => {
@@ -20,7 +21,7 @@ const FormInput = props => {
 
         actionsAnimal.addAnimal(animals, form)
     }
-    
+
 
     return (
         <div>
@@ -93,10 +94,105 @@ const FormInput = props => {
                             onChange={(e) => actionForm.changeStatus(e.target.value)}
                         />
                     </div>
-                    <div className="text-center">
-                        <button className="btn btn-primary my-1" type="submit" onClick={addAnimal}>ADD</button>
-                    </div>
+
                 </form>
+            </div>
+
+            <div>
+                <div className="row">
+                    <div className="col-sm-3 mt-5"></div>
+                    <div className="col-sm-6 mt-5 card">
+                        <div className="card-body ml-3 mr-3 mt-5 mb-1">
+                            <form>
+                                <h1 className="Topic">ADD YOUR DATA</h1>
+                                <div className="form-group">
+                                    <label htmlFor="username">IMAGE</label>
+                                    <input type="text"
+                                        name="imgUrlUser"
+                                        className="form-control"
+                                        id="imgUrlUser"
+                                        onChange={(e) => actionForm.changeImgurlUser(e.target.value)}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="username">USER NAME</label>
+                                    <input type="text"
+                                        name="username"
+                                        className="form-control"
+                                        id="username"
+                                        onChange={(e) => actionForm.changeNameUser(e.target.value)}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="email">Email*</label>
+                                    <input type="email"
+                                        name="email"
+                                        className="form-control"
+                                        id="email"
+                                        onChange={(e) => actionForm.changeEmailUser(e.target.value)}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="text">Telephone</label>
+                                    <input type="text"
+                                        name="telephone"
+                                        className="form-control"
+                                        id="telephone"
+                                        onChange={(e) => actionForm.changeTelephoneUser(e.target.value)}
+
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="text">Facebook</label>
+                                    <input type="text"
+                                        name="facebook"
+                                        className="form-control"
+                                        id="facebook"
+                                        onChange={(e) => actionForm.changeFacebookUser(e.target.value)}
+
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="email">Line</label>
+                                    <input type="text"
+                                        name="line"
+                                        className="form-control"
+                                        id="line"
+                                        onChange={(e) => actionForm.changeLineUser(e.target.value)}
+
+                                    />
+                                </div>
+                                <Form.Group controlId="formGridAddress2">
+                                    <Form.Label>Address</Form.Label>
+                                    <Form.Control placeholder="Apartment, studio, or floor"   onChange={(e) => actionForm.changeAddressUser(e.target.value)}/>
+                                </Form.Group>
+
+                                <Form.Row>
+                                    <Form.Group as={Col} controlId="formGridCity">
+                                        <Form.Label>City</Form.Label>
+                                        <Form.Control  onChange={(e) => actionForm.changeCityUser(e.target.value)} />
+                                    </Form.Group>
+
+                                    <Form.Group as={Col} controlId="formGridState">
+                                        <Form.Label>State</Form.Label>
+                                        <Form.Control  onChange={(e) => actionForm.changeStateUser(e.target.value)} />
+                                    </Form.Group>
+                                    <Form.Group controlId="formGridZip">
+                                        <Form.Label>Zip</Form.Label>
+                                        <Form.Control  onChange={(e) => actionForm.changeZipUser(e.target.value)} />
+                                    </Form.Group>
+                                </Form.Row>
+                                <Form.Group id="formGridCheckbox">
+                                    <Form.Check type="checkbox" label="Check me out" />
+                                </Form.Group>
+                                <div className="text-center">
+                                    <button className="btn btn-primary my-1" type="submit" onClick={addAnimal}>ADD</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="col-sm-3 mt-5"></div>
+                </div>
             </div>
         </div>
     );
