7c9de0bca06ff18808d028fac41d629794c8ea30
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-05 22:29:08 +0700

    test

--- server/animals.js
+++ server/animals.js
@@ -149,6 +149,6 @@ router.route('/auth/psu')
 
 app.use("*", (req, res) => res.status(404).send('404 Not found'));
 
-app.listen(80, () => { console.log('server is running PORT 8000') })
+app.listen(80, () => { console.log('server is running PORT 80') })
 
 
