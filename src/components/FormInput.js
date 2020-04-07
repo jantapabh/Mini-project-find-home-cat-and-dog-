179e11aea2864af7daadde18ab5cf3e86d6b215c
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-07 20:05:56 +0700

    add commit

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -6,11 +6,89 @@ import { useSelector, useDispatch } from 'react-redux'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {firestore} from '../index.js'
+import Animal from './Animal'
 
 
 
 
 const FormInput = props => {
+
+    const [id, setId] = useState(0)
+    const [imgUrl1, setImgUrl1] = useState('')
+    const [imgUrl2, setImgUrl2] = useState('')
+    const [imgUrl3, setImgUrl3] = useState('')
+    const [strain, setStrain] = useState('')
+    const [name, setName] = useState('')
+    const [old, setOld] = useState(0)
+    const [habits, setHabits] = useState('')
+    const [because, setBecause] = useState('')
+    const [status, setStatus] = useState('')
+    const [imgUrlUser, setImgUrlUser] = useState('')
+    const [nameUser, setNameUser] = useState('')
+    const [email, setEmail] = useState('')
+    const [telephone, setTelephone] = useState('')
+    const [facebook, setFacebook] = useState('')
+    const [line, setLine] = useState('')
+    const [address, setAddress] = useState('')
+    const [city, setCity] = useState('')
+    const [state, setState] = useState('')
+    const [zip, setZip] = useState('')
+    const [animal, setAnimal] = useState([{}])
+
+    useEffect(() => {
+
+        retriveData()
+
+
+    }, [])
+
+
+    const retriveData = () => {
+
+        firestore.collection("animals").onSnapshot(snapshot => {
+
+            console.log(snapshot);
+
+            let myAnimal = snapshot.docs.map(d => {
+                const { id, imgUrl1, imgUrl2, imgUrl3, strain, name, old, habits, because, status, imgUrlUser, nameUser, email, telephone, facebook, line, address, city, state, zip } = d.data()
+                console.log(id, imgUrl1, imgUrl2, imgUrl3, strain, name, old, habits, because, status, imgUrlUser, nameUser, email, telephone, facebook, line, address, city, state, zip)
+                return { id, imgUrl1, imgUrl2, imgUrl3, strain, name, old, habits, because, status, imgUrlUser, nameUser, email, telephone, facebook, line, address, city, state, zip }
+            })
+
+            setAnimal(myAnimal)
+          
+
+        })
+    }
+
+    const renderAnimal = () => {
+        console.log(animal)
+        if (animal && animal.length) {
+            return animal.map((animal, index) => {
+                return (
+
+                    <Animal key={index} animal={animal}
+
+                    />
+
+                )
+            })
+
+        }
+        else {
+
+            return <li>No Animal</li>
+
+        }
+    }
+
+    const addAnimals = () => {
+
+        let id = (animal.length === 0) ? 1 : animal[animal.length - 1].id + 1
+        firestore.collection("animals").doc(id + '').set({ id, imgUrl1, imgUrl2, imgUrl3, strain, name, old, habits, because, status, imgUrlUser, nameUser, email, telephone, facebook, line, address, city, state, zip })
+
+    }
+
 
     const form = useSelector(state => state.form)
     const dispatch = useDispatch();
@@ -43,24 +121,24 @@ const FormInput = props => {
     const [myAnimal, setMyAnimal] = useState([])
     
 
-    const retriveData = () => {
+    // const retriveData = () => {
 
-        firestore.collection("animals").onSnapshot(( snapshot) => {
+    //     firestore.collection("animals").onSnapshot(( snapshot) => {
 
-           let myAni =  snapshot.docs.map(d => {
+    //        let myAni =  snapshot.docs.map(d => {
 
-                const {id , imgUrl1, imgUrl2, imgUrl3, strain, name, old, habits, because, status, imgUrlUser, nameUser, email, telephone, facebook, line, address, city, state, zip} = d.data()
-                console.log(id , imgUrl1, imgUrl2, imgUrl3, strain, name, old, habits, because, status, imgUrlUser, nameUser, email, telephone, facebook, line, address, city, state, zip)
-                return {id , imgUrl1, imgUrl2, imgUrl3, strain, name, old, habits, because, status, imgUrlUser, nameUser, email, telephone, facebook, line, address, city, state, zip}
+    //             const {id , imgUrl1, imgUrl2, imgUrl3, strain, name, old, habits, because, status, imgUrlUser, nameUser, email, telephone, facebook, line, address, city, state, zip} = d.data()
+    //             console.log(id , imgUrl1, imgUrl2, imgUrl3, strain, name, old, habits, because, status, imgUrlUser, nameUser, email, telephone, facebook, line, address, city, state, zip)
+    //             return {id , imgUrl1, imgUrl2, imgUrl3, strain, name, old, habits, because, status, imgUrlUser, nameUser, email, telephone, facebook, line, address, city, state, zip}
 
-            })
+    //         })
 
-            setMyAnimal(myAni);
-           //  console.log(myAni)
-        })
+    //         setMyAnimal(myAni);
+    //        //  console.log(myAni)
+    //     })
 
         
-    }
+    // }
     
     const getAnimal = async () => {
 
@@ -305,7 +383,7 @@ const FormInput = props => {
                 </TabPane>
             </TabContent>
             <div className="text-center">
-            <Button color="primary" onClick={addAnimal} style={{ margin: 15, padding: 10}}>ADD DOG OR CAT AND YOUR DATA</Button>
+            <Button color="primary" onClick={addAnimals} style={{ margin: 15, padding: 10}}>ADD DOG OR CAT AND YOUR DATA</Button>
             </div>
         </div>
 
