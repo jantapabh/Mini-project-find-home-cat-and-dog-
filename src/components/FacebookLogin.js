4ec725d4ce0de6ed66bb5fd9a93c14607156635b
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-09 04:04:02 +0700

    add facebooklogin

--- src/components/FacebookLogin.js
+++ src/components/FacebookLogin.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import fire from '../config/fire';
+import { AuthActions } from '../redux/store';
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux'
+import { Button, Form, Card } from 'react-bootstrap';
+
+const FacebookLogin = props => {
+
+    const actions = bindActionCreators({ ...AuthActions }, useDispatch())
+    const [facebookLink, setFacebookLink] = useState('');
+
+    const getFacebookLink = async () => {
+
+        const res = await axios.get(`http://localhost/api/auth/facebook`);
+        setFacebookLink(res.data);
+
+    }
+
+    useEffect(() => {
+
+        getFacebookLink()
+
+    }, []);
 
 
-
-const FacebookLogin = () => {
-
-
-    return(
+    return (
         <div>
-            </div>
+             <Card style={{ display: 'flex', justifyContent: 'center' }}>
+                            <Card.Body>
+                                <Card.Title className="topic">Facebook LOGIN</Card.Title>
+                                <br />
+                                <br />
+                        
+                                <Button href={facebookLink}>LOGIN</Button>
+                                <br />
+                                <br />
+                            </Card.Body>
+                        </Card>
+        </div>
     )
 }
 
