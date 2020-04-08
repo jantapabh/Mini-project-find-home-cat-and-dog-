edb3ad6c6d8466326c8663d0b223bbf8dad843d2
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-08 22:46:00 +0700

    add commit login and logout

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,11 +3,38 @@ import './Login.css'
 import axios from 'axios';
 import './Form.css'
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBInput } from 'mdbreact';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import fire from '../config/fire';
 
 
+
+
+
+
+function Copyright() {
+
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
 
 
 class Login extends Component {
@@ -58,51 +85,85 @@ class Login extends Component {
     }
 
     render() {
+
+
+
         return (
-
-            <div>
-                <form>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="text"
-                        label="EMAIL"
-                        name="email"
-                        autoComplete="text"
-                        autoFocus
-                        value={this.state.email}
-                        onChange={this.handleChange}
-
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        value={this.state.password}
-                        onChange={this.handleChange}
-
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
-                    <div className="text-center">
-                        <button className="btn btn-primary my-1" type="submit" onClick={this.login}>Login</button>
-                        <button className="btn btn-primary my-1" type="submit" onClick={this.signup}>Signup</button>
-                        <div className="row my-3 d-flex justify-content-center">
-                        </div>
+            <Grid container component="main" className="root">
+                <CssBaseline />
+                <Grid item className="image" >
+                    <img src="https://cdn.pixabay.com/photo/2015/12/13/20/16/cats-1091652_960_720.jpg" style={{ width: 700, height: 500, margin: 20, padding: 10}} />
+                </Grid>
+                <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                    <div className="paper">
+                        <Avatar className="avatar">
+                            <LockOutlinedIcon style={{ backgroundColor: 'green'}}/>
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Login
+                </Typography>
+                        <form className="form" noValidate>
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                value={this.state.email}
+                                onChange={this.handleChange}
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                                value={this.state.password}
+                                onChange={this.handleChange}
+                            />
+                            <FormControlLabel
+                                control={<Checkbox value="remember" color="primary" />}
+                                label="Remember me"
+                            />
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                color="primary"
+                                className="submit"
+                                onClick={this.login}
+                                style={{ margin: 5, width: 100 }}
+                            >
+                                Log in
+                  </Button>
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                color="primary"
+                                className="submit"
+                                onClick={this.signup}
+                                style={{ margin: 5, width: 100 }}
+                            >
+                                Sign Up
+                  </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                      </Link>
+                                </Grid>
+                            </Grid>
+                        </form>
                     </div>
-                </form>
-            </div>
-
+                </Grid>
+            </Grid>
         )
     }
 
