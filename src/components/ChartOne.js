70e45aa861d91e4b0d0bf71b23c23ab70ab9c5e7
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-10 13:59:57 +0700

    add commit

--- src/components/ChartOne.js
+++ src/components/ChartOne.js
@@ -1,70 +1,274 @@
+import FusionCharts from "fusioncharts";
+import charts from "fusioncharts/fusioncharts.charts";
+import ReactFusioncharts from "react-fusioncharts";
 
-      
-        var options = {
-          series: [44, 55, 13, 33],
-          chart: {
-          width: 380,
-          type: 'donut',
+// Resolves charts dependancy
+charts(FusionCharts);
+
+const dataSource = {
+  chart: {
+    caption: "Sales of Cigarettes in Europe",
+    subcaption: "(per adult per day)",
+    legendposition: "BOTTOM",
+    entitytooltext: "$lname: <b>$datavalue </b>cigarettes",
+    legendcaption: "Number of cigarettes smoked per adult per day",
+    entityfillhovercolor: "#FFCDD2",
+    theme: "fusion"
+  },
+  colorrange: {
+    gradient: "0",
+    color: [
+      {
+        maxvalue: "4",
+        displayvalue: "2-4",
+        code: "#EF9A9A"
+      },
+      {
+        maxvalue: "6",
+        displayvalue: "4-6",
+        code: "#EF5350"
+      },
+      {
+        maxvalue: "8",
+        displayvalue: "6-8",
+        code: "#E53935"
+      },
+      {
+        maxvalue: "10",
+        displayvalue: "8-10",
+        code: "#C62828"
+      },
+      {
+        maxvalue: "11",
+        displayvalue: "No data available",
+        code: "#FFEBEE"
+      }
+    ]
+  },
+  data: [
+    {
+      data: [
+        {
+          id: "021",
+          value: "11",
+          tooltext: "No data available"
         },
-        dataLabels: {
-          enabled: false
+        {
+          id: "002",
+          value: "11",
+          tooltext: "No data available"
         },
-        responsive: [{
-          breakpoint: 480,
-          options: {
-            chart: {
-              width: 200
-            },
-            legend: {
-              show: false
-            }
-          }
-        }],
-        legend: {
-          position: 'right',
-          offsetY: 0,
-          height: 230,
+        {
+          id: "025",
+          value: "11",
+          tooltext: "No data available"
+        },
+        {
+          id: "034",
+          value: "11",
+          tooltext: "No data available"
+        },
+        {
+          id: "027",
+          value: "11",
+          tooltext: "No data available"
+        },
+        {
+          id: "043",
+          value: "11",
+          tooltext: "No data available"
+        },
+        {
+          id: "028",
+          value: "11",
+          tooltext: "No data available"
+        },
+        {
+          id: "044",
+          value: "11",
+          tooltext: "No data available"
+        },
+        {
+          id: "026",
+          value: "11"
+        },
+        {
+          id: "023",
+          value: "11",
+          tooltext: "No data available"
+        },
+        {
+          id: "010",
+          value: "6.4"
+        },
+        {
+          id: "017",
+          value: "4.5"
+        },
+        {
+          id: "042",
+          value: "5.7"
+        },
+        {
+          id: "018",
+          value: "6.4"
+        },
+        {
+          id: "030",
+          value: "4.9"
+        },
+        {
+          id: "039",
+          value: "2.9"
+        },
+        {
+          id: "029",
+          value: "6.4"
+        },
+        {
+          id: "005",
+          value: "7"
+        },
+        {
+          id: "013",
+          value: "5.1"
+        },
+        {
+          id: "038",
+          value: "7.1"
+        },
+        {
+          id: "032",
+          value: "5.4"
+        },
+        {
+          id: "040",
+          value: "6.7"
+        },
+        {
+          id: "003",
+          value: "6.5"
+        },
+        {
+          id: "037",
+          value: "6.1"
+        },
+        {
+          id: "008",
+          value: "6.4"
+        },
+        {
+          id: "006",
+          value: "11",
+          tooltext: "No data available"
+        },
+        {
+          id: "001",
+          value: "11",
+          tooltext: "No data available"
+        },
+        {
+          id: "015",
+          value: "9.9"
+        },
+        {
+          id: "045",
+          value: "11",
+          tooltext: "No data available"
+        },
+        {
+          id: "007",
+          value: "7.6"
+        },
+        {
+          id: "024",
+          value: "11",
+          tooltext: "No data available"
+        },
+        {
+          id: "033",
+          value: "6"
+        },
+        {
+          id: "016",
+          value: "7"
+        },
+        {
+          id: "036",
+          value: "7.8"
+        },
+        {
+          id: "009",
+          value: "6.2"
+        },
+        {
+          id: "014",
+          value: "6.1"
+        },
+        {
+          id: "031",
+          value: "6.5"
+        },
+        {
+          id: "041",
+          value: "11",
+          tooltext: "No data available"
+        },
+        {
+          id: "004",
+          value: "5.4"
+        },
+        {
+          id: "022",
+          value: "5.3"
+        },
+        {
+          id: "020",
+          value: "3.5"
+        },
+        {
+          id: "011",
+          value: "4.8"
+        },
+        {
+          id: "012",
+          value: "4"
+        },
+        {
+          id: "046",
+          value: "8.3"
+        },
+        {
+          id: "019",
+          value: "5.6"
+        },
+        {
+          id: "035",
+          value: "11",
+          tooltext: "No data available"
+        },
+        {
+          id: "047",
+          value: "11",
+          tooltext: "No data available"
         }
-        };
+      ]
+    }
+  ]
+};
 
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-        chart.render();
-      
-      
-        function appendData() {
-        var arr = chart.w.globals.series.slice()
-        arr.push(Math.floor(Math.random() * (100 - 1 + 1)) + 1)
-        return arr;
-      }
-      
-      function removeData() {
-        var arr = chart.w.globals.series.slice()
-        arr.pop()
-        return arr;
-      }
-      
-      function randomize() {
-        return chart.w.globals.series.map(function() {
-            return Math.floor(Math.random() * (100 - 1 + 1)) + 1
-        })
-      }
-      
-      function reset() {
-        return options.series
-      }
-      
-      document.querySelector("#randomize").addEventListener("click", function() {
-        chart.updateSeries(randomize())
-      })
-      
-      document.querySelector("#add").addEventListener("click", function() {
-        chart.updateSeries(appendData())
-      })
-      
-      document.querySelector("#remove").addEventListener("click", function() {
-        chart.updateSeries(removeData())
-      })
-      
-      document.querySelector("#reset").addEventListener("click", function() {
-        chart.updateSeries(reset())
-      })
+class ChartOne extends React.Component {
+  render() {
+    return (
+      <ReactFusioncharts
+        type="europe"
+        width="100%"
+        height="100%"
+        dataFormat="JSON"
+        dataSource={dataSource}
+      />
+    );
+  }
+}
+
+export default ChartOne;
