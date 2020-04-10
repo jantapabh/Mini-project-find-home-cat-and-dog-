511d79b8d2d66afc9aa03ed0ee261d20158cf48e
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-10 22:34:48 +0700

    add comiit

--- src/components/News.js
+++ src/components/News.js
@@ -8,9 +8,9 @@ import {
 const News = (props) => {
     return (
         <div>
-            <CardGroup style={{ margin: 5, padding: 5, height: 400, height: 400}}>
+            <CardGroup style={{ margin: 5, padding: 5, width: 1500, height: 400}}>
             <Card  style={{ margin: 10, padding: 10}}>
-                <CardImg top width="100%" src="http://phufay.go.th/public/list_upload/backend/list_615/pics_topic_615.jpg?672" style={{ width: 400, height: 150}} />
+                <CardImg top width="100%" src="http://phufay.go.th/public/list_upload/backend/list_615/pics_topic_615.jpg?672" style={{ width: 450, height: 150}} />
                 <CardBody> 
                     <CardTitle>
                         <h4>โรคพิษสุนัขบ้าน่ากลัวกว่าที่คิด</h4>
@@ -20,7 +20,7 @@ const News = (props) => {
                 </CardBody>
             </Card>
             <Card  style={{ margin: 10, padding: 10}}>
-                <CardImg top width="100%" src="https://image.shutterstock.com/image-photo/happy-hipster-couple-bulldog-relaxing-260nw-713750788.jpg" style={{ width: 400, height: 160}} />
+                <CardImg top width="100%" src="https://image.shutterstock.com/image-photo/happy-hipster-couple-bulldog-relaxing-260nw-713750788.jpg" style={{ width: 450, height: 160}} />
                 <CardBody>
                     <CardTitle>
                         <h4>ระบบสารสนเทศเพื่อการเฝ้าระวังโรคพิษสุนัขบ้า</h4>
@@ -30,7 +30,7 @@ const News = (props) => {
                 </CardBody>
             </Card>
             <Card  style={{ margin: 10}}>
-                <CardImg top width="100%" src="https://image.bangkokbiznews.com/kt/media/image/news/2019/01/23/825021/750x422_825021_1548238139.jpg" style={{ width: 400, height: 150}} />
+                <CardImg top width="100%" src="https://image.bangkokbiznews.com/kt/media/image/news/2019/01/23/825021/750x422_825021_1548238139.jpg" style={{ width: 450, height: 150}} />
                 <CardBody>
                     <CardTitle><h4>"ปศุสัตว์"เผยไทยจะมีสุนัข-แมวจรจัด 1.92 ล้านตัวในปี 70 </h4></CardTitle>
                     <CardText>ผลการสำรวจในปี 2550 ประเทศไทยมีสุนัขจรจัด ประมาณ 350,000 ตัว ดังนั้นหากมีการเพิ่มประชากรสุนัขและแมวจรจัดคาดว่า ในปี พ.ศ. 2570 ประเทศไทยจะมีสุนัขและแมวจรจัด ประมาณ 1.92 ล้านตัว</CardText>
--- src/components/TabTwo.js
+++ src/components/TabTwo.js
@@ -9,30 +9,33 @@ const TapTwo = (props) => {
     return (
         <CardGroup style={{ margin: 5, padding: 5, height: 400, height: 400}}>
             <Card  style={{ margin: 10, padding: 10}}>
-                <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                <CardImg top width="100%" src="https://www.santisookdogandcat.org/img/logo-top.png" style={{ width: 400, height: 150}}  />
                 <CardBody>
-                    <CardTitle>Card title</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
-                    <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                    <Button>Button</Button>
+                    <CardTitle>
+                       <h4>มูลนิธิสันติสุขเพื่อสุนัขและแมวจรจัด</h4>
+                        </CardTitle>
+                    <CardText>มูลนิธิสันติสุขเพื่อสุนัขและแมวจรจัดมีกิจกรรมมากมายในการช่วยเหลือสุนัขและแมวจรจัดและนอกจากนั้นยังมีการทำบุญและบริจาคของเพื่อสมทบทุนให้กับมูลนิธิต่อไปด้วยค่ะ</CardText>
+                    <Button href="https://www.santisookdogandcat.org/">อ่านพิ่มเติม</Button>
                 </CardBody>
             </Card>
             <Card  style={{ margin: 10, padding: 10}}>
-                <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                <CardImg top width="100%" src="https://home4animals.org/wp-content/uploads/2018/02/5.jpg" style={{ width: 450, height: 150}}  />
                 <CardBody>
-                    <CardTitle>Card title</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
-                    <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-                    <Button>Button</Button>
+                <CardTitle>
+                       <h4>มูลนิธิบ้านสงเคราะห์สัตว์พิการ</h4>
+                        </CardTitle>
+                    <CardText>มูลนิธิบ้านสงเคราะห์สัตว์พิการ มีจุดเริ่มต้นจากครอบครัวคุณพิมพ์กุล โอฬารศิรโรจน์ (เรณู จุลสุคนธ์) ที่ได้ช่วยเหลือสัตว์ต่างๆ ที่พิการจนหายจากการเจ็บป่วย </CardText>
+                    <Button href="https://home4animals.org/history/">อ่านพิ่มเติม</Button>
                 </CardBody>
             </Card>
             <Card  style={{ margin: 10, padding: 10}}>
-                <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                <CardImg top width="100%" src="/assets/318x180.svg" style={{ width: 400, height: 150}}  />
                 <CardBody>
-                    <CardTitle>Card title</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardTitle>
+                       <h4>มูลนิธิสันติสุขเพื่อสุนัขและแมวจรจัด</h4>
+                        </CardTitle>
                     <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-                    <Button>Button</Button>
+                    <Button>อ่านพิ่มเติม</Button>
                 </CardBody>
             </Card>
             
