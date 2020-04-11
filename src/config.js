7375133e4c65d38c59bc46061d7242c5ed502e74
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-11 21:07:41 +0700

    add commit

--- src/config.js
+++ src/config.js
@@ -11,15 +11,6 @@ const config = {
   appId: "1:855817765425:web:b943ea1362874b54b3cb10",
   measurementId: "G-Z66DSKN8SP"
 
-  // apiKey: "AIzaSyBK29PZlEcHMwQsXpJpgOKd5fTErKSi8EE",
-  // authDomain: "fir-react-auth-97868.firebaseapp.com",
-  // databaseURL: "https://fir-react-auth-97868.firebaseio.com",
-  // projectId: "fir-react-auth-97868",
-  // storageBucket: "fir-react-auth-97868.appspot.com",
-  // messagingSenderId: "890209709881",
-  // appId: "1:890209709881:web:094d0f03b7b7a4188882e8",
-  // measurementId: "G-CF9B4J586Y"
-
 
 };
 
