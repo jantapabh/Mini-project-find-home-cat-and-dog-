028ffb93f1d8f083d7a7eac34b1a7fb332a32ab9
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-11 21:21:58 +0700

    add cmmit firebase

--- src/config.js
+++ src/config.js
@@ -2,14 +2,23 @@
 
 const config = {
 
-  apiKey: "AIzaSyD4HL7WzNIO2qyB4yRsaULj6K0Rx_sSQrA",
-  authDomain: "cat-mini-project.firebaseapp.com",
-  databaseURL: "https://cat-mini-project.firebaseio.com",
-  projectId: "cat-mini-project",
-  storageBucket: "cat-mini-project.appspot.com",
-  messagingSenderId: "855817765425",
-  appId: "1:855817765425:web:b943ea1362874b54b3cb10",
-  measurementId: "G-Z66DSKN8SP"
+  // apiKey: "AIzaSyD4HL7WzNIO2qyB4yRsaULj6K0Rx_sSQrA",
+  // authDomain: "cat-mini-project.firebaseapp.com",
+  // databaseURL: "https://cat-mini-project.firebaseio.com",
+  // projectId: "cat-mini-project",
+  // storageBucket: "cat-mini-project.appspot.com",
+  // messagingSenderId: "855817765425",
+  // appId: "1:855817765425:web:b943ea1362874b54b3cb10",
+  // measurementId: "G-Z66DSKN8SP"
+
+  apiKey: "AIzaSyBK29PZlEcHMwQsXpJpgOKd5fTErKSi8EE",
+  authDomain: "fir-react-auth-97868.firebaseapp.com",
+  databaseURL: "https://fir-react-auth-97868.firebaseio.com",
+  projectId: "fir-react-auth-97868",
+  storageBucket: "fir-react-auth-97868.appspot.com",
+  messagingSenderId: "890209709881",
+  appId: "1:890209709881:web:094d0f03b7b7a4188882e8",
+  measurementId: "G-CF9B4J586Y"
 
 
 };
