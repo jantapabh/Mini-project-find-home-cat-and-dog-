5317a7a5afd5345ed99001e34e88eb3bffb37117
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-12 16:29:51 +0700

    add commit

--- src/components/FormInputTwo.js
+++ src/components/FormInputTwo.js
@@ -156,7 +156,7 @@ const useStyles = makeStyles((theme) => ({
                     <Toolbar>
                         <CameraIcon className={classes.icon} />
                         <Typography variant="h6" color="inherit" noWrap>
-                           กรอกข้อมูลาำหรับประกาศหา
+                           กรอกข้อมูลสำหรับประกาศหา
           </Typography>
                     </Toolbar>
                 </AppBar>
