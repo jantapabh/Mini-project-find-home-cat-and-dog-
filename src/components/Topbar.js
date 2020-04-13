18d4e96777fe9a3ac327768b983bf914ed6b2055
jantapabh <s6035512034@phuket.psu.ac.th>
2020-04-14 00:37:40 +0700

    add comimt

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -2,47 +2,21 @@ import React, { Component, useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo1 from '../static/images/logo1.png'
 import './Topbar.css'
-import { Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Form, Nav, NavDropdown, Navbar, FormControl, Spinner, Icon } from 'react-bootstrap';
-import { ArrowRight } from 'react-bootstrap-icons';
-import { MDBRow, MDBCol, MDBIcon, MDBBtn, MDBListGroupItem } from "mdbreact";
-import axios from 'axios'
 import Avatar from '@material-ui/core/Avatar';
 import fire from '../config/fire';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
-import firebase from 'firebase'
-
-
-axios.defaults.withCredentials = true
-
-const color = 'blue'
-
-
-class Topbar extends Component {
 
 
 
-    constructor(props) {
-
-        super(props)
-
-        this.state = {
-
-            color: ''
-        }
-
-    }
-
-    logout(e) {
-
-        fire.auth().signOut();
-
-    }
 
 
+const Topbar = () => {
 
-    render() {
+const logout = e => {
+
+    fire.auth().signOut();
+}
 
 
         return (
@@ -70,7 +44,7 @@ class Topbar extends Component {
                                 <img src="https://www.wisible.io/wp-content/uploads/2019/08/avatar-human-male-profile-user-icon-518358.png" style={{ width: 50, height: 50}}/>
                             </Avatar>
                              <NavDropdown title="USER" style={{ fontSize: 20, margin: 1, padding: 5 }} id="basic-nav-dropdown">
-                                <NavDropdown.Item style={{ fontSize: 15 }} onClick={this.logout}>LOGOUT</NavDropdown.Item>
+                                <NavDropdown.Item style={{ fontSize: 15 }} onClick={logout}>LOGOUT</NavDropdown.Item>
                                 <NavDropdown.Divider />
                             </NavDropdown>
                         </Form>
@@ -80,5 +54,5 @@ class Topbar extends Component {
 
         );
     }
-}
+
 export default Topbar;
